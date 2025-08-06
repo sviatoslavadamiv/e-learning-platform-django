186250af2ff67d65ca68df70771db36a0a75241e
sviatoslav <svatik5.adamiv@gmail.com>
2025-08-07 00:14:10 +0200

    Basic CRUD functionality implemented to Course model

--- educa/educa/urls.py
+++ educa/educa/urls.py
@@ -1,8 +1,10 @@
+from xml.etree.ElementInclude import include
+
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path(
@@ -16,6 +18,7 @@ urlpatterns = [
         name='logout',
     ),
     path('admin/', admin.site.urls),
+    path('course/', include('courses.urls'))
 ]
 
 if settings.DEBUG:
