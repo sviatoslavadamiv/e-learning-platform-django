2f7d1ad4296cc3fa017640c3896e3e79f5337b28
sviatoslav <svatik5.adamiv@gmail.com>
2025-08-06 20:29:38 +0200

    Created models that are suitable for adding diverse content to the course modules

--- educa/courses/models.py
+++ educa/courses/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 
 class Subject(models.Model):
     title = models.CharField(max_length=200)
@@ -44,3 +46,47 @@ class Module(models.Model):
 
     def __str__(self):
         return self.title
+
+class Content(models.Model):
+    module = models.ForeignKey(
+        Module,
+        related_name='contents',
+        on_delete=models.CASCADE,
+    )
+    content_type = models.ForeignKey(
+        ContentType,
+        on_delete=models.CASCADE,
+        limit_choices_to={
+            'model__in':('text', 'file', 'image', 'video')
+        }
+    )
+    object_id = models.PositiveIntegerField()
+    item = GenericForeignKey('content_type', 'object_id')
+
+class ItemBase(models.Model):
+    owner = models.ForeignKey(
+        User,
+        related_name='%(class)s_related',
+        on_delete=models.CASCADE,
+    )
+    title = models.CharField(max_length=250)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.title
+
+class Text(ItemBase):
+    content = models.TextField()
+
+class File(ItemBase):
+    content = models.FileField(upload_to='files')
+
+class Image(ItemBase):
+    content = models.FileField(upload_to='images')
+
+class Video(ItemBase):
+    url = models.URLField()
